dd88b1495103f651b5dc8426f209cc68a058b09a
rijans <makwanarijans@gmail.com>
2025-07-11 12:50:41 +0530

    adding login functionalities

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,3 +37,30 @@ exports.register = async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 };
+
+exports.login =async(req,res)=>{
+  const {email,password}=req.body;
+  try {
+    const user = await User.findOne({email})    
+    if(!user) return res.status(400).json({message:"Invalid Credentials"});
+
+const isMatch= await bcrypt.compare(password,user.password)
+ if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+ const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d"
+    });
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token
+    });
+
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
